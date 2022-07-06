7b09878f7ea15701744128db1177f2de3541d4c3
khosta77 <puwerfulpants@gmail.com>
2022-07-06 17:18:27 +0300

    Прогресс есть. Отрисовывает графики в новых окнах

--- main.cpp
+++ main.cpp
@@ -1,12 +1,30 @@
 #include <GL/glut.h>
 #include <cmath>
-#include <thread>
 #include <iostream>
-#include <unistd.h>
+#include <functional>
+#include <vector>
+#include <map>
 
 using namespace std;
+
 //class StepanPlot {
+//private:
+//    vector<int> mainWindow;
+//
+//    void
 //public:
+//    StepanPlot(int argc, char** argv) {
+//        glutInit(&argc, argv);
+//        glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB);
+//        glutInitWindowSize(1024, 512);
+//        glutInitWindowPosition(40, 40);
+//    }
+//
+//    void plot(vector<double> x, vector<double> y) {
+//        mainWindow.push_back(glutCreateWindow("Test"));
+//        glutDisplayFunc(display);
+//        init();
+//    }
 //};
 
 static auto func_cos(double x) {
@@ -18,8 +36,14 @@ static auto func_sin(double x) {
 }
 
 static double (*func)(double);
+//std::vector<std::function<void(int)>> functions;
+
+vector<pair<vector<double>, vector<double>>> df;
+vector<int> window;
+
 
 void display() {
+    int win = glutGetWindow() - 1;
     glClear(GL_COLOR_BUFFER_BIT);
     glBegin(GL_LINES);
     {
@@ -33,38 +57,66 @@ void display() {
     glEnd();
     glBegin(GL_POINTS);
     glColor3f(1, .2, 0);
-    for (double x = 0; x < 450; x+=0.1) {
-        glVertex2d(x - 0.1, func_cos(x - 0.1));
-        glVertex2d(x, func(x));
+    for (double i = 1; i < df[win].first.size(); i++) {
+        glVertex2d(df[win].first[i - 1], df[win].second[i - 1]);
+        glVertex2d(df[win].first[i], df[win].second[i]);
     }
     glEnd();
     glFlush();
-    sleep(1);
-    glutDestroyWindow(0);
 }
 
 void init() {
-    glClearColor(0.25, 0.0, 0.2, 1.0);
+    glClearColor(1.0, 1.0, 1.0, 1.0);
     glColor3f(0.6, 1.0, 0.0);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
     glOrtho(-20.0, 500.0, -1.2, 1.2, 0.0, 100.0);
 }
 
-void test(int argc, char** argv, double (*function)(double)) {
-    func = function;
+void plot(vector<double> x, vector<double> y, const char *name) {
+    window.push_back(glutCreateWindow(name));
+    df.push_back(pair<vector<double>, vector<double>>(x, y));
+    glutDisplayFunc(display);
+    init();
+}
+
+void test(int argc, char** argv/*, double (*function)(double)*/) {
     glutInit(&argc, argv);
     glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB);
     glutInitWindowSize(1024, 512);
     glutInitWindowPosition(40, 40);
-    glutCreateWindow("Test");
-    glutDisplayFunc(display);
-    init();
-    glutMainLoop();
+//    int mainWindow = glutCreateWindow("Test");
+//    glutDisplayFunc(display);
+//    init();
+////    glutMainLoop();
+//
+//    //дочерние окна
+//    int subWindow1 = glutCreateWindow("Test2");
+//
+//    glutDisplayFunc(display);
+//    init();
+
+//    glutMainLoop();
 }
 
 int main(int argc, char** argv) {
-    test(argc, argv, func_cos);
-//    test(argc, argv, func_cos);
+    vector<double> X;
+    vector<double> Y1, Y2;
+
+    for (double i = 0.1; i <= 500; i+=0.1) {
+        X.push_back(i);
+        Y1.push_back(func_cos(i));
+        Y2.push_back(func_sin(i));
+    }
+
+//    for (int i = 0; i < X.size(); i++) {
+//        cout << i << ") " << X[i] << " " << Y1[i] << " " << Y1[i] << endl;
+//    }
+
+    test(argc, argv);
+    plot(X, Y1, "cos");
+    plot(X, Y2, "sin");
+    glutMainLoop();
+
     return 0;
 }
