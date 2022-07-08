00246b91959fd4a71212c689a27b879c496807da
khosta77 <puwerfulpants@gmail.com>
2022-07-08 18:28:14 +0300

    Проблема с выводом текста - на данном этапе нельзя решить

--- main.cpp
+++ main.cpp
@@ -6,10 +6,11 @@
 #include <map>
 
 // TODO: на 08.08
-//  2) Красивая отрисовки сетки
-//  3) Возможность писать текст на графике
-//  4) Рисовать оси, если они попадают
 //  5) ...
+
+/*TODO:
+ * Нельзя просто так взять и сделать вывод текста
+ * */
 
 using namespace std;
 
@@ -164,6 +165,49 @@ public:
 
     static void display() {
         currentInstance->draw();
+    }
+
+    void RenderText(Shader &s, std::string text, GLfloat x, GLfloat y, GLfloat scale, glm::vec3 color)
+    {
+        // Activate corresponding render state
+        s.Use();
+        glUniform3f(glGetUniformLocation(s.Program, "textColor"), color.x, color.y, color.z);
+        glActiveTexture(GL_TEXTURE0);
+        glBindVertexArray(VAO);
+
+        // Iterate through all characters
+        std::string::const_iterator c;
+        for (c = text.begin(); c != text.end(); c++)
+        {
+            Character ch = Characters[*c];
+
+            GLfloat xpos = x + ch.Bearing.x * scale;
+            GLfloat ypos = y - (ch.Size.y - ch.Bearing.y) * scale;
+
+            GLfloat w = ch.Size.x * scale;
+            GLfloat h = ch.Size.y * scale;
+            // Update VBO for each character
+            GLfloat vertices[6][4] = {
+                    { xpos, ypos + h, 0.0, 0.0 },
+                    { xpos, ypos, 0.0, 1.0 },
+                    { xpos + w, ypos, 1.0, 1.0 },
+                    { xpos, ypos + h, 0.0, 0.0 },
+                    { xpos + w, ypos, 1.0, 1.0 },
+                    { xpos + w, ypos + h, 1.0, 0.0 }
+            };
+            // Render glyph texture over quad
+            glBindTexture(GL_TEXTURE_2D, ch.textureID);
+            // Update content of VBO memory
+            glBindBuffer(GL_ARRAY_BUFFER, VBO);
+            glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vertices), vertices);
+            glBindBuffer(GL_ARRAY_BUFFER, 0);
+            // Render quad
+            glDrawArrays(GL_TRIANGLES, 0, 6);
+            // Now advance cursors for next glyph (note that advance is number of 1/64 pixels)
+            x += (ch.Advance >> 6) * scale; // Bitshift by 6 to get value in pixels (2^6 = 64)
+        }
+        glBindVertexArray(0);
+        glBindTexture(GL_TEXTURE_2D, 0);
     }
 
     void init_display_mode() {
