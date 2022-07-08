bdb6510abd59fb4b4e8f5251e9de7e46702b7132
khosta77 <puwerfulpants@gmail.com>
2022-07-08 18:31:56 +0300

    Проблема с выводом текста - на данном этапе нельзя решить, вообще нельзя

--- main.cpp
+++ main.cpp
@@ -167,49 +167,6 @@ public:
         currentInstance->draw();
     }
 
-    void RenderText(Shader &s, std::string text, GLfloat x, GLfloat y, GLfloat scale, glm::vec3 color)
-    {
-        // Activate corresponding render state
-        s.Use();
-        glUniform3f(glGetUniformLocation(s.Program, "textColor"), color.x, color.y, color.z);
-        glActiveTexture(GL_TEXTURE0);
-        glBindVertexArray(VAO);
-
-        // Iterate through all characters
-        std::string::const_iterator c;
-        for (c = text.begin(); c != text.end(); c++)
-        {
-            Character ch = Characters[*c];
-
-            GLfloat xpos = x + ch.Bearing.x * scale;
-            GLfloat ypos = y - (ch.Size.y - ch.Bearing.y) * scale;
-
-            GLfloat w = ch.Size.x * scale;
-            GLfloat h = ch.Size.y * scale;
-            // Update VBO for each character
-            GLfloat vertices[6][4] = {
-                    { xpos, ypos + h, 0.0, 0.0 },
-                    { xpos, ypos, 0.0, 1.0 },
-                    { xpos + w, ypos, 1.0, 1.0 },
-                    { xpos, ypos + h, 0.0, 0.0 },
-                    { xpos + w, ypos, 1.0, 1.0 },
-                    { xpos + w, ypos + h, 1.0, 0.0 }
-            };
-            // Render glyph texture over quad
-            glBindTexture(GL_TEXTURE_2D, ch.textureID);
-            // Update content of VBO memory
-            glBindBuffer(GL_ARRAY_BUFFER, VBO);
-            glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vertices), vertices);
-            glBindBuffer(GL_ARRAY_BUFFER, 0);
-            // Render quad
-            glDrawArrays(GL_TRIANGLES, 0, 6);
-            // Now advance cursors for next glyph (note that advance is number of 1/64 pixels)
-            x += (ch.Advance >> 6) * scale; // Bitshift by 6 to get value in pixels (2^6 = 64)
-        }
-        glBindVertexArray(0);
-        glBindTexture(GL_TEXTURE_2D, 0);
-    }
-
     void init_display_mode() {
         glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
         glutInitWindowSize(fr.WIDTH, fr.HEIGHT);
@@ -377,7 +334,7 @@ int main(int argc, char** argv) {
     vector<double> X;
     vector<double> Y1, Y2, Y3;
 
-    for (double i = -100.0; i <= 500.1; i+=0.1) {
+    for (double i = -100.0; i <= 100.1; i+=0.1) {
         X.push_back(i);
         Y1.push_back(func_cos(i));
         Y2.push_back(func_sin(i));
