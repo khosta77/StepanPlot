667022a6c14ee28b6a6500153fa66911abcbe097
khosta77 <puwerfulpants@gmail.com>
2022-07-09 19:32:21 +0300

    Добавлены все типы данных которые на данном этапе используются

--- project/include/utilities/StepanPlot_data_frame.h
+++ project/include/utilities/StepanPlot_data_frame.h
@@ -1,6 +1,8 @@
 #ifndef PLOT_TEST_STEPANPLOT_DATA_FRAME_H
 #define PLOT_TEST_STEPANPLOT_DATA_FRAME_H
 
+#include "StepanPlot_dop_func.h"
+
 namespace stepan_plot {
 
     namespace data_frame {
@@ -20,10 +22,7 @@ namespace stepan_plot {
              * \param height высота
              * \param width широта
              */
-            Frame(unsigned int height, unsigned int width) {
-                h = height;
-                w = width;
-            }
+            Frame(unsigned int height, unsigned int width) : h(height), w(width) {}
         };
 //------------------------------------------------------------------------------------------------------------
         /**\brief - класс позиции нового окна
@@ -46,10 +45,7 @@ namespace stepan_plot {
              * \param x смещение по х
              * \param y смещение по y
              */
-            Position(unsigned int x, unsigned int y) {
-                X = x;
-                Y = y;
-            }
+            Position(unsigned int x, unsigned int y) : X(x), Y(y) {}
 
             /** \brief Переход к следующему положению
              * \param fr рамка
@@ -75,13 +71,67 @@ namespace stepan_plot {
          * */
         class Ortho {
         public:
-            double left;
-            double right;
-            double bottom;
-            double top;
-            double near = 0.0;
-            double far = 100.0;
+            double l;
+            double r;
+            double b;
+            double t;
+            double n = 0.0;
+            double f = 100.0;
+
+            Ortho() {}
+
+            Ortho(double left, double right, double bottom, double top, double near = 0.0, double far = 100.0)
+               : l(left), r(right), b(bottom), t(top), n(near), f(far) {}
+
+            Ortho(std::vector<double> X, std::vector<double> Y) {
+                try {
+                    if (X.empty() || Y.empty()) {
+                        throw Exception("---> X or Y empty\n");
+                    }
+
+                    if (X.size() != Y.size()) {
+                        throw Exception("---> X != Y\n");
+                    }
+                } catch(Exception& e) {
+                    std::cout << e.what() << std::endl;
+                }
+
+                l = af::min_elem(X);
+                r = af::max_elem(X);
+                b = af::min_elem(Y);
+                t = af::max_elem(Y);
+            }
         };
+//------------------------------------------------------------------------------------------------------------
+        class Brush {
+        public:
+            double r;
+            double b;
+            double g;
+
+            Brush() {
+                r = af::rand_factor(1.0, 0.0);
+                b = af::rand_factor(1.0, 0.0);
+                g = af::rand_factor(1.0, 0.0);
+            }
+
+            Brush(double red, double blue, double green) : r(red), b(blue), g(green) {}
+        };
+//------------------------------------------------------------------------------------------------------------
+        class plot_frame {
+        public:
+//            std::string name;  |* Имя графика *|
+            std::pair<std::vector<double>, std::vector<double>> XOY;  /* Оси Х и У */
+            Ortho ort_XOY;  /* Рамка для каждого графика */
+            Brush br;  /* Цвет линии */
+            bool grid_status = false;  /* Сетка графика */
+
+            plot_frame() {}
+
+            plot_frame(std::pair<std::vector<double>, std::vector<double>> xoy, Ortho ort, Brush pb = Brush())
+               : XOY(xoy), ort_XOY(ort), br(pb) {}
+        };
+//------------------------------------------------------------------------------------------------------------
     }
 
 };
