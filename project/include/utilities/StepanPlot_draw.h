c10c31b7b0881a2f3f792d384d9a5521e55f3651
khosta77 <puwerfulpants@gmail.com>
2022-07-10 13:01:30 +0300

    Функции рисования выносятся в отдельный файл.

---
+++ project/include/utilities/StepanPlot_draw.h
@@ -0,0 +1,17 @@
+#ifndef PLOT_TEST_STEPANPLOT_DRAW_H
+#define PLOT_TEST_STEPANPLOT_DRAW_H
+
+namespace stepan_plot {
+
+    namespace draw_func {
+
+        class Draw {
+        public:
+
+        };
+
+    };
+
+};
+
+#endif //PLOT_TEST_STEPANPLOT_DRAW_H
